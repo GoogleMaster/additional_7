b357ebe725c052cf82339209e3aa16a8b9227e47
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-02-26 00:27:53 +0300

    what the hell is going on? (all tests done???)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+    /*var table = [
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]],
+        [[],[],[],[],[],[],[],[],[]]
+    ]; //arrays in array will contain list of suitable numbers
+
+    for(var row = 0; row < 9; row++){
+         for(var col = 0; col < 9; col++){
+             if(matrix[row][col] != 0){
+                 table[row][col] = matrix[row][col];
+             }
+         }
+    }
+    return table;*/
+
+    //little hack :D
+    for(var row = 0; row < 9; row++){
+        for(var col = 0; col < 9; col++){
+            if(matrix[row][col] == 0){
+                matrix[row][col] = [];
+            }
+        }
+    }
+    return matrix;
 }
